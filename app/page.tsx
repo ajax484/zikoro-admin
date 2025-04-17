cd1ad39a1b556528988a129aceb9f966d2904e6a
EmmanuelN1 <emmanuelnzeakor00@yahoo.com>
2025-04-17 11:04:59 +0100

    fix: minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,7 @@ export default function Page() {
 
   return (
     <div className="flex items-center justify-center mx-auto h-screen text-center">
-      {user && (
-        <div className="">
-          <h1 className="text-3xl">
-            Welcome <span className="font-bold">{user?.firstName}</span>{" "}
-          </h1>
-          <p className="text-base mt-6 cursor-pointer" onClick={() => logOut()}>
-            <Logout size={18} /> Logout
-          </p>
-        </div>
-      )}
+        
     </div>
   );
 }
