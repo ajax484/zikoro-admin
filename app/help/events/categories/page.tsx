ced5ec6e4cb2356018dd5bc6eeb518acc55d0d53
EmmanuelN1 <emmanuelnzeakor00@yahoo.com>
2025-05-01 17:08:57 +0100

    category page

---
+++ app/help/events/categories/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import EventsCategories from "@/components/eventsHelp/EventsCategories";
+
+export default function page() {
+  return (
+    <div className="bg-[#F7F8FF] min-h-screen ">
+      <EventsCategories />
+    </div>
+  );
+}
--- components/eventsHelp/EventsCategories.tsx
+++ components/eventsHelp/EventsCategories.tsx
@@ -11,8 +11,14 @@ import {
 import Link from "next/link";
 import { GreaterThan } from "styled-icons/fa-solid";
 
-export default function EventsHelp() {
+export default function EventsCategories() {
   const datas = [
+    {
+      title: "Category",
+      date: "1/11/2025",
+      articleNo: 120,
+      viewNo: 120,
+    },
     {
       title: "Category",
       date: "1/11/2025",
@@ -48,14 +54,25 @@ export default function EventsHelp() {
   return (
     <div className="pt-[40px] px-3 lg:px-[56px]">
       {/* top */}
-      <div className=" flex items-center gap-x-1 ">
-        <p className="text-[#555555] font-semibold text-opacity-50 text-[14px]">
-          Help center <GreaterThan size={14} />
-        </p>
-        <p className="text-[#555555] font-semibold text-[14px] text-opacity-100">
-          {" "}
-          Zikoro Events
-        </p>
+      <div className="flex flex-col lg:flex-row gap-y-4 justify-between w-full items-center">
+        {/* left */}
+        <div className=" w-full flex items-center gap-x-1 ">
+          <p className="text-[#555555] font-semibold text-opacity-50 text-[14px]">
+            Help center <GreaterThan size={14} /> Zikoro Events{" "}
+            <GreaterThan size={14} />
+          </p>
+          <p className="text-[#555555] font-semibold text-[14px] text-opacity-100">
+            {" "}
+            Categories
+          </p>
+        </div>
+        {/* right */}
+        <div className=" w-full h-10 items-center flex justify-end ">
+          <button className=" text-white bg-gradient-to-tr from-custom-gradient-start to-custom-gradient-end py-[10px] px-4 rounded-[10px] flex gap-x-1 text-base font-semibold items-center h-full">
+            {" "}
+            Write an article <HelpPencil />
+          </button>
+        </div>
       </div>
 
       {/* mid */}
@@ -65,22 +82,26 @@ export default function EventsHelp() {
           <div className="flex justify-center mx-auto">
             <div className="">
               <p className="text-center font-semibold text-[20px]">
-                Zikoro Events
+                Categories
+              </p>
+
+              <p className="text-center font-medium text-[14px]">
+                Last updated: 1/11/2025{" "}
               </p>
               <div className="flex gap-x-4">
                 <div className="flex gap-x-1 text-[12px] font-medium items-center">
-                  <HelpFolder />
+                  <HelpArticle />
                   <div className="flex gap-x-1">
-                    <p>100</p>
-                    <p>Categories</p>
+                    <p>6</p>
+                    <p>Articles</p>
                   </div>
                 </div>
                 <div className="flex gap-x-1 text-[12px] font-medium items-center">
                   <HelpEyeIcon />
 
                   <div className="flex gap-x-1">
-                    <p>120</p>
-                    <p>Articles</p>
+                    <p>720</p>
+                    <p>Total Reads</p>
                   </div>
                 </div>
               </div>
@@ -88,38 +109,27 @@ export default function EventsHelp() {
           </div>
 
           {/* 2nd section */}
-          <div className="h-[33px] flex flex-col lg:flex-row items-center gap-6 mt-4">
+          <div className=" flex flex-col justify-center items-center gap-6 mt-5 lg:mt-[52px]">
             {/* left */}
-            <div className="w-full lg:w-[50%] ">
-              <input
-                type="text"
-                placeholder="Search"
-                className="pl-3 py-2 w-full bg-transparent text-[14px] border-b-[1px] outline-none border-[#EAEAEA]"
-              />
-            </div>
+            <p className=" text-[#31353B] text-center text-base font-medium">
+              Articles in this category{" "}
+            </p>
+
             {/* right */}
-            <div className="flex justify-between gap-x-1 w-full lg:w-[50%] items-center ">
+            <div className="flex justify-between items-center gap-x-6 ">
+              <div className="w-full lg:w-[536px] ">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="pl-3 py-2 w-full bg-transparent text-[14px] border-b-[1px] outline-none border-[#EAEAEA]"
+                />
+              </div>
               <HelpFilter />
-              <button className=" text-white bg-gradient-to-tr from-custom-gradient-start to-custom-gradient-end py-[10px] px-4 rounded-[10px] flex gap-x-1 text-base font-semibold items-center">
-                {" "}
-                Write an article <HelpPencil />
-              </button>
             </div>
           </div>
 
-          <p className="mt-24 lg:mt-8 text-[#31353B] text-center text-base font-medium">
-            Group related articles with category{" "}
-          </p>
-
           {/* bottom */}
-          <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-4 w-full">
-            <div className="flex flex-col items-center justify-center w-full  gap-2 py-[36px] bg-gradient-to-tr from-custom-gradient-start to-custom-gradient-end rounded-[10px] cursor-pointer ">
-              <HelpWhiteFileIcon />
-              <p className="text-base font-semibold text-white">
-                Create a new category
-              </p>
-            </div>
-
+          <div className="mt-6 grid grid-cols-1 lg:grid-cols-2 gap-4 w-full">
             {datas.map((data, index) => (
               <div className=" bg-white rounded-[10px] p-3">
                 <Link href="/help/events/categories">
