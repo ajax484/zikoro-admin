d8c41f277dba14e6595ac0f01a882eaadb8c634e
EmmanuelN1 <emmanuelnzeakor00@yahoo.com>
2025-05-18 15:23:00 +0100

    fix: routing problems

--- app/help/bookings/customizing/page.tsx
+++ app/help/bookings/customizing/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { BooksCustomize } from "@/constants";
+import BooksCustomize from "@/components/booksHelp/BooksCustomize";
 
 export default function page() {
   return (
     <div className="bg-[#F7F8FF] min-h-screen ">
-      <BooksCustomize />
+      <BooksCustomize/>
     </div>
   );
 }
